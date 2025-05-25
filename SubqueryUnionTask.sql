858d9a7342ab14417a463bb51cbdf295d4bf8e9f
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-05-25 13:49:40 +0400

    Complete inserting data into tables

--- SubqueryUnionTask.sql
+++ SubqueryUnionTask.sql
@@ -11,4 +11,31 @@
 -- Your goal is to:
 -- • Compare the data of both groups.
 -- • Clean or restructure the database safely.
--- • Explore more advanced SQL topics on your own (subqueries, transactions).
+-- • Explore more advanced SQL topics on your own (subqueries, transactions).-- create database Create database SubqueryUnionTask;use SubqueryUnionTask-- Tables
+
+-- Trainees Table
+CREATE TABLE Trainees (
+ TraineeID INT PRIMARY KEY,
+ FullName VARCHAR(100),
+ Email VARCHAR(100),
+ Program VARCHAR(50),
+ GraduationDate DATE
+);
+-- Job Applicants Table
+CREATE TABLE Applicants (
+ ApplicantID INT PRIMARY KEY,
+ FullName VARCHAR(100),
+ Email VARCHAR(100),
+ Source VARCHAR(20), -- e.g., "Website", "Referral"
+ AppliedDate DATE
+);--------------------------- Sample Data
+-- Insert into Trainees
+INSERT INTO Trainees VALUES
+(1, 'Layla Al Riyami', 'layla.r@example.com', 'Full Stack .NET', '2025-04-30'),
+(2, 'Salim Al Hinai', 'salim.h@example.com', 'Outsystems', '2025-03-15'),
+(3, 'Fatma Al Amri', 'fatma.a@example.com', 'Database Admin', '2025-05-01');
+-- Insert into Applicants
+INSERT INTO Applicants VALUES
+(101, 'Hassan Al Lawati', 'hassan.l@example.com', 'Website', '2025-05-02'),
+(102, 'Layla Al Riyami', 'layla.r@example.com', 'Referral', '2025-05-05'), -- same person as trainee
+(103, 'Aisha Al Farsi', 'aisha.f@example.com', 'Website', '2025-04-28');
