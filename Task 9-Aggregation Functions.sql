4d1b0e2eaf870428b3c59b1201b7defae3d67877
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-05-25 09:27:01 +0400

    Task 9-Aggregation Functions - Library Database

--- Task 9-Aggregation Functions.sql
+++ Task 9-Aggregation Functions.sql
@@ -99,3 +99,22 @@ HAVING SUM(Amount) > 200000;
 
 ---------------------------
 
+--Library Database
+use LibraryDatabase;
+--26. Count total books in Books table.
+SELECT COUNT(*) AS total_books FROM Book;
+--27. Average book price from Books table.
+SELECT AVG(Price) AS average_price FROM Book;
+--28. Count books per library grouped by Library_ID.
+SELECT L_ID, COUNT(*) AS book_count
+FROM LibraryBook
+GROUP BY L_ID;
+--29. Count books borrowed per member from Borrows table grouped by Member_ID.
+SELECT M_ID, COUNT(*) AS books_borrowed
+FROM Book
+GROUP BY M_ID;
+--30. List members who borrowed more than 3 books grouped by Member_ID.
+SELECT M_ID, COUNT(*) AS books_borrowed
+FROM Book
+GROUP BY M_ID
+HAVING COUNT(*) > 3;
